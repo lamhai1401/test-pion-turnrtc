51b2bbf4a8bd4e9922312431c6d3bd43d7f71de0
Dat Vo <thanhdat1710@gmail.com>
2019-10-03 16:03:51 +0700

    update server

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const wss1 = new WebSocket.Server({ noServer: true });
 
 server.on('upgrade', function upgrade(request, socket, head) {
   const pathname = url.parse(request.url).pathname;
-  const query = {...querystring.parse(url.parse(request.url).query)}
+  const query = { ...querystring.parse(url.parse(request.url).query) }
 
   wss1.handleUpgrade(request, socket, head, function done(ws) {
     wss1.emit('connection', ws, request, { pathname, query });
@@ -29,23 +29,33 @@ wss1.on(
   'connection',
   function connection(/**@type {WebSocket} */ws, request, { pathname, query }) {
     let id = query.id
-    
+    let pingInterval = setInterval(
+      () => ws.ping(),
+      5000
+    )
     if (!clientMap.has(id)) {
       let s = new Set()
       clientMap.set(id, s)
     }
-  
+
     clientMap.get(id).add(ws)
 
-    ws.on("close", () => clientMap.get(id).delete(ws))
+    ws.on("close", () => {
+      clientMap.get(id).delete(ws)
+      clearInterval(pingInterval)
+    })
 
-    ws.on("message", function(event) {
-      let [targetId, message] = JSON.parse(event)
-      for (let soc of clientMap.get(targetId) || []) {
-        if(targetId != id){
-          console.log(id,"->",targetId)
-          soc.send(JSON.stringify([id,message]))
+    ws.on("message", function (event) {
+      try {
+        let [targetId, message] = JSON.parse(event)
+        for (let soc of clientMap.get(targetId) || []) {
+          if (targetId != id) {
+            console.log(id, "->", targetId)
+            soc.send(JSON.stringify([id, message]))
+          }
         }
+      } catch (error) {
+
       }
     })
 
