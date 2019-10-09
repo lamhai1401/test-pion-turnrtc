a4b11ffa00f97600d9c1dd412a56d20bc44cc390
Dat Vo <thanhdat1710@gmail.com>
2019-10-09 16:05:01 +0700

    update

--- web/sdpTransform.js
+++ web/sdpTransform.js
@@ -1,104 +1,133 @@
 function convert_str_to_list(sdp_string, character) {
-  return sdp_string.split(character)
+    return sdp_string.split(character)
 }
 
 function convert_list_to_string(sdp_list, character) {
-  return sdp_list.join(character)
+    return sdp_list.join(character)
 }
 
 function sdp_filter(sdp_raw, engine) {
-  /**
-  sdp_raw : {
-      a=rtpmap:96 VP8/90000
-      a=rtcp-fb:96 goog-remb
-      a=rtcp-fb:96 transport-cc
-      a=rtcp-fb:96 ccm fir
-      a=rtcp-fb:96 nack
-      a=rtcp-fb:96 nack pli
-      }
-  engine : VP8/H.264/....
-   * 
-   */
+    /**
+    sdp_raw : {
+        a=rtpmap:96 VP8/90000
+        a=rtcp-fb:96 goog-remb
+        a=rtcp-fb:96 transport-cc
+        a=rtcp-fb:96 ccm fir
+        a=rtcp-fb:96 nack
+        a=rtcp-fb:96 nack pli
+        }
+    engine : VP8/H.264/....
+     * 
+     */
 
-  let sdp_temp = []
+    let sdp_temp = []
 
-  let sdp_list = convert_str_to_list(sdp_raw, "\r\n")
-  let engine_number = null
+    let sdp_list = convert_str_to_list(sdp_raw, "\r\n")
+    let engine_number = null
 
-  let rtp_map = "a=rtpmap:"
+    let rtp_map = "a=rtpmap:"
 
-  let rtcp_fb = "a=rtcp-fb:"
-  let rtcp_fb_vp8 = null
+    let rtcp_fb = "a=rtcp-fb:"
+    let rtcp_fb_vp8 = null
 
-  let fmtp = "a=fmtp:"
-  let fmtp_vp8 = null
+    let fmtp = "a=fmtp:"
+    let fmtp_vp8 = null
 
-  let media_video = "m=video"
-  let is_video_format = false
-  let is_first_rtx = true
+    let media_video = "m=video"
+    let is_video_format = false
+    let is_first_rtx = true
 
-  //  for line in sdp_raw
-  for (let i = 0; i < sdp_list.length; i++) {
-      if (sdp_list[i].startsWith("m=")) {
-          if (sdp_list[i].startsWith(media_video)) {
-              is_video_format = true
-              sdp_temp.push(sdp_list[i])
-          }
-          else {
-              is_video_format = false
-              sdp_temp.push(sdp_list[i])
-          }
-      }
-      else if (sdp_list[i].startsWith(rtp_map)) {
-          if (!is_video_format) {
-              sdp_temp.push(sdp_list[i])
-          }
-          else if (sdp_list[i].includes(engine)) {
-              var numberPattern = /\d+/g;
-              engine_number = sdp_list[i].match(numberPattern)[0]
-              sdp_temp.push(sdp_list[i])
+    //  for line in sdp_raw
+    for (let i = 0; i < sdp_list.length; i++) {
+        if (sdp_list[i].startsWith("m=")) {
+            if (sdp_list[i].startsWith(media_video)) {
+                is_video_format = true
+                sdp_temp.push(sdp_list[i])
+            }
+            else {
+                is_video_format = false
+                sdp_temp.push(sdp_list[i])
+            }
+        }
+        else if (sdp_list[i].startsWith(rtp_map)) {
+            if (!is_video_format) {
+                sdp_temp.push(sdp_list[i])
+            }
+            else if (sdp_list[i].includes(engine)) {
+                var numberPattern = /\d+/g;
+                engine_number = sdp_list[i].match(numberPattern)[0]
+                sdp_temp.push(sdp_list[i])
 
-              rtcp_fb_vp8 = rtcp_fb + engine_number
-              fmtp_vp8 = fmtp_vp8 + engine_number
-          }
-          else if (sdp_list[i].includes("rtx") && engine_number != null && is_first_rtx) {
-              sdp_temp.push(sdp_list[i])
-              is_first_rtx = false
-          }
-          else {
-              continue
-          }
-      }
-      else if (sdp_list[i].startsWith(rtcp_fb)) {
-          if (rtcp_fb_vp8 && sdp_list[i].startsWith(rtcp_fb_vp8)) {
-              sdp_temp.push(sdp_list[i])
-          }
-          else {
-              continue
-          }
-      }
-      else if (sdp_list[i].startsWith(fmtp)) {
-          if (fmtp_vp8 && sdp_list[i].startsWith(fmtp_vp8)) {
-              sdp_temp.push(sdp_list[i])
-          }
-          else if (engine_number && sdp_list[i].includes(engine_number)) {
-              sdp_temp.push(sdp_list[i])
-          }
-          else {
-              continue
-          }
-      }
-      else {
-          sdp_temp.push(sdp_list[i])
-      }
-  }
-  let new_sdp = convert_list_to_string(sdp_temp, "\r\n").replace(new RegExp(engine_number, 'g'), 96)
-  let engine_number2 = parseInt(engine_number) + 1
-  return new_sdp.replace(new RegExp(engine_number2 + "", 'g'), 97)
+                rtcp_fb_vp8 = rtcp_fb + engine_number
+                fmtp_vp8 = fmtp_vp8 + engine_number
+            }
+            else if (sdp_list[i].includes("rtx") && engine_number != null && is_first_rtx) {
+                sdp_temp.push(sdp_list[i])
+                is_first_rtx = false
+            }
+            else {
+                continue
+            }
+        }
+        else if (sdp_list[i].startsWith(rtcp_fb)) {
+            if (rtcp_fb_vp8 && sdp_list[i].startsWith(rtcp_fb_vp8)) {
+                sdp_temp.push(sdp_list[i])
+            }
+            else {
+                continue
+            }
+        }
+        else if (sdp_list[i].startsWith(fmtp)) {
+            if (fmtp_vp8 && sdp_list[i].startsWith(fmtp_vp8)) {
+                sdp_temp.push(sdp_list[i])
+            }
+            else if (engine_number && sdp_list[i].includes(engine_number)) {
+                sdp_temp.push(sdp_list[i])
+            }
+            else {
+                continue
+            }
+        }
+        else {
+            sdp_temp.push(sdp_list[i])
+        }
+    }
+    let new_sdp = convert_list_to_string(sdp_temp, "\r\n").replace(new RegExp(engine_number, 'g'), 96)
+    let engine_number2 = parseInt(engine_number) + 1
+    return new_sdp.replace(new RegExp(engine_number2 + "", 'g'), 97)
 }
-
+const params = {
+    audioSendBitrate: undefined,
+    audioSendCodec: undefined,
+    audioRecvBitrate: undefined,
+    audioRecvCodec: undefined,
+    opusMaxPbr: undefined,
+    opusFec: undefined,
+    opusDtx: undefined,
+    opusStereo: undefined,
+    videoSendBitrate: undefined,
+    videoSendInitialBitrate: undefined,
+    videoSendCodec: undefined,
+    videoRecvBitrate: undefined,
+    videoRecvCodec: "VP8",
+    videoFec: undefined,
+    mediaConstraints: {
+        audio: true, video: true
+    }
+}
 const sdpTransform = (sdp) => {
-  console.log(sdp)
-  return sdp_filter(sdp, "VP8")
+    try {
+        sdp = maybeSetOpusOptions(sdp, params);
+        sdp = maybePreferAudioSendCodec(sdp, params);
+        sdp = maybePreferVideoSendCodec(sdp, params);
+        sdp = maybeSetAudioSendBitRate(sdp, params);
+        sdp = maybeSetVideoSendBitRate(sdp, params);
+        sdp = maybeSetVideoSendInitialBitRate(sdp, params);
+        sdp = maybeRemoveVideoFec(sdp, params)
+        return sdp
+    } catch (error) {
+        console.error(error)
+        return sdp
+    }
 }
 
